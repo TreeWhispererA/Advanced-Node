384665ac664d61dca652e128b109b64a0b83766a
TreeWhispererA <139618051+TreeWhispererA@users.noreply.github.com>
2023-07-20 22:13:57 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// run `node index.js` in the terminal
+// Async or Q
+function register(name, password, cb) {
+  checkIfNameExists(name, function (err, result) {
+    if (err) {
+      return cb('error');
+    }
+    checkIfPasswordGood(password, function (err, result) {
+      if (err) {
+        return cb('error');
+      }
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+      createAccount(name, password, function (err, result) {
+        if (err) {
+          return cb('error');
+        }
+        createBlog(name, function (err, result) {
+          sendEmail(name, function (err, result) {
+            callback(result);
+          });
+        });
+      });
+    });
+  });
+}
